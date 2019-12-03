bfa11408aafe8be3fe3ae003a770f2a1ab7ba6e7
Twaver <twaver@Twavers-MacBook-Air.local>
2019-12-03 10:56:12 +0800

    skm上传测试

---
+++ src/pages/home/test.vue
@@ -0,0 +1,55 @@
+<script lang="ts">
+/* COMPONENT DOCUMENT
+ * author: 
+ * date: 2019/12/03
+ * desc: 
+ */
+
+import { Component, Emit, Prop, Vue, Watch } from 'vue-property-decorator';
+import { Getter, Action } from 'vuex-class';
+
+
+@Component({
+  name: '',
+  components: {},
+})
+export default class  extends Vue {
+  /* ------------------------ INPUT & OUTPUT ------------------------ */
+  // @Prop() private parentData!: any;
+  // @Emit('event_name') private handler() {}
+
+  /* ------------------------ VUEX (vuex getter & vuex action) ------------------------ */
+  // @Getter private some_getter!: any;
+  // @Action private some_action!: () => void;
+
+  /* ------------------------ LIFECYCLE HOOKS (created & mounted & ...) ------------------------ */
+  // private created() {}
+  // private mounted() {}
+
+  /* ------------------------ COMPONENT STATE (data & computed & model) ------------------------ */
+  // private my_data: string = 'some thing'; // data
+  // get computed_data(): string { return 'computed' } // computed
+
+  /* ------------------------ WATCH ------------------------ */
+  // @Watch('some_thing') private some_thing_changed(val: any, oldVal: any) {}
+
+  /* ------------------------ METHODS ------------------------ */
+  // private some_method(): void {}
+
+}
+
+</script>
+
+<template>
+<div class="module_test">
+
+</div>
+</template>
+
+<style lang="stylus" scoped>
+// @import '~@/assets/stylus/var'
+
+.module_test
+  pass
+
+</style>
