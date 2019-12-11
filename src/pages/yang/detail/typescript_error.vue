4697b171033a911583d6abab2d8fad36fe41095c
zhaoyang <zhaoyang@educationtek.com>
2019-12-11 15:37:26 +0800

    '记录ts错误忽略'

---
+++ src/pages/yang/detail/typescript_error.vue
@@ -0,0 +1,61 @@
+<script lang="ts">
+/* COMPONENT DOCUMENT
+ * author: zhaoyang
+ * date: 2019/12/11
+ * desc: 屁阳专属
+ */
+
+import { Component, Emit, Prop, Vue, Watch } from 'vue-property-decorator';
+import { Getter, Action } from 'vuex-class';
+
+@Component({
+  name: 'ts-error',
+  components: {
+  },
+})
+export default class TsError extends Vue {
+  /* ------------------------ INPUT & OUTPUT ------------------------ */
+  // @Prop() private parentData!: any;
+  // @Emit('event_name') private handler() {}
+
+  /* ------------------------ VUEX (vuex getter & vuex action) ------------------------ */
+  // @Getter private some_getter!: any;
+  // @Action private some_action!: () => void;
+
+  /* ------------------------ LIFECYCLE HOOKS (created & mounted & ...) ------------------------ */
+  private created() {
+  }
+  // private mounted() {}
+
+  /* ------------------------ COMPONENT STATE (data & computed & model) ------------------------ */
+  /* ------------------------ WATCH ------------------------ */
+  // @Watch('some_thing') private some_thing_changed(val: any, oldVal: any) {}
+
+  /* ------------------------ METHODS ------------------------ */
+  // Typescript错误忽略
+  // 单行忽略
+  // @ts-ignore
+
+  // 忽略全文
+  // @ts-nocheck
+
+  // 取消忽略全文
+  // @ts-check
+}
+
+</script>
+
+<template>
+<layout>
+  <div class="common_page_container module_ts_error">
+  </div>
+</layout>
+</template>
+
+<style lang="stylus" scoped>
+// @import '~@/assets/stylus/var'
+
+.module_ts_error
+  pass
+
+</style>
