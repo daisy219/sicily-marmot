f3c978deeb1725ce99d5cf74101d6c96f1b3e66c
skm <15221955725@163.com>
2020-05-28 13:41:00 +0800

    修改打包问题

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   // 打包输出文件夹
   outputDir: 'dist',
   // 部署应用包时的基本 URL
-  publicPath:  process.env.NODE_ENV === 'production' ? 'http://47.96.2.170/dist/' : './',
+  publicPath:  process.env.NODE_ENV === 'production' ? 'http://47.96.2.170/' : './',
   // server 配置
   devServer: {
       host: '0.0.0.0',
